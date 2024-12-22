661ab9281277f2bdfd891662c96ac41d464ad135
Eugene Azimut <33938421+yazimut@users.noreply.github.com>
2024-12-22 13:55:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ccraft
+ccraft is a modern automated build system for C/C++ projects, similar to GNU Make, but simpler and more powerful
---
+++ ccraft/__main__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'launch'
+]
+
+import os
+
+def launch():
+    print(os.getcwd())
+
+if __name__ == '__main__':
+    launch()
